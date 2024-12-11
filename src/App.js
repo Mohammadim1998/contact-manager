ce6dc15ebe26b66c84819f92e3900e2c759762b0
unknown <m.pourhosein7777@gmail.com>
2024-12-11 11:59:27 +0330

    intial contact project

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import Contacts from './components/contact/Contacts';
+import Navbar from './components/Navbar';
+
+const App = () => {
+
+  return (
+    <div className="App">
+      <Navbar />
+      <Contacts />
+    </div>
+  );
+}
+
+export default App;
