6d2ec34f60cf8ef390b5b8f57131793482f2e468
unknown <m.pourhosein7777@gmail.com>
2024-12-16 11:39:19 +0330

    add routes

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import Contact from './components/contact/Contact';
 import AddContact from './components/contact/AddContact';
 import EditContact from './components/contact/EditContact';
 import ViewContact from './components/contact/ViewContact';
-
+import { Navigate, Route, Routes } from 'react-router-dom';
 
 const App = () => {
   const [loading, setLoading] = useState(false);
@@ -15,6 +15,13 @@ const App = () => {
   return (
     <div className="App">
       <Navbar />
+      <Routes>
+        <Route path='/' element={<Navigate to={"/contacts"} />} />
+        <Route path='/contacts' element={<Contacts contacts={contacts} loading={loading} />} />
+        <Route path='/contacts/:contactId' element={<Contact />} />
+        <Route path='/contacts/add' element={<AddContact />} />
+        <Route path='/contacts/edit/:contactId' element={<EditContact />} />
+      </Routes>
       <Contacts contacts={contacts} loading={loading} />
     </div>
   );
